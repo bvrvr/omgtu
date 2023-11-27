f67ece9995d5a3295506e3339dca3915ae5a4f18
Varvara Buryak <buryak.varvara@gmail.com>
2023-11-27 21:00:49 +0600

    Add files via upload

---
+++ алгоритмизация и программирование/30.10.23_lab.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30._10._23_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+
+            int sum = 0;
+
+            Console.WriteLine("Введите текст:");
+            string s = Console.ReadLine();
+
+            foreach (char ch in s)
+            {
+                if (char.IsDigit(ch) && ch % 2 == 0)
+                {
+                    sum++;
+                }
+            }
+
+            if (sum == 0)
+            {
+                Console.WriteLine("В этом тексте нет чётных цифр.");
+            }
+
+            else
+            {
+                Console.WriteLine($"В этом тексте {sum} чётных цифр.");
+            }
+
+            //Задание 2
+
+            Console.WriteLine("Введите текст:");
+            string str = Console.ReadLine();
+
+            string strWithoutSpaces = str.Replace(" ", "");
+
+            string lowerStr = strWithoutSpaces.ToLower();
+
+            char[] charStr = lowerStr.ToCharArray();
+            Array.Reverse(charStr);
+
+            string reversedStr = new string(charStr);
+
+            if (lowerStr == reversedStr)
+            {
+                Console.WriteLine("Это палиндром.");
+            }
+
+            else
+            {
+                Console.WriteLine("Это не палиндром.");
+            }
+        }
+    }
+}
