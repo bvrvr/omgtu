3bd4e64d047c5c4a439e869296f28c863a8e4903
Varvara Buryak <buryak.varvara@gmail.com>
2024-01-05 23:00:20 +0600

    Add files via upload

---
+++ алгоритмизация и программирование/25.12.23.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._12._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double maxd = 0;
+            for (int i = 106732567; i <= 152673836; i++)
+            {
+                double sqri = Math.Sqrt(i);
+                double numdel = 0;
+                if (Math.Round(sqri) == sqri)
+                {
+                    maxd = 1;
+                    for (int j = 2; j < Math.Round(sqri); j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            if (maxd == 1)
+                            {
+                                maxd = i / j;
+                                maxd = Math.Round(maxd);
+                            }
+
+                            numdel += 2;
+
+                        }
+                        if (numdel == 2) { Console.WriteLine($"{i} \t {maxd} "); }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ алгоритмизация и программирование/27.11.23_practice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._11._23_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int minADiff = Int32.MaxValue;
+            int aDiff = 0;
+            int sumA = 0;
+            int sumB = 0;
+            int minSumB = 0;
+            int minSumA = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] pair = Console.ReadLine().Split(' ');
+
+                int a = Convert.ToInt32(pair[0]);
+                int b = Convert.ToInt32(pair[1]);
+
+                aDiff += (a - b);
+                sumA += a;
+                minSumB += b;
+                sumB += b;
+
+                if (aDiff < minADiff)
+                {
+                    minADiff = aDiff;
+                    minSumB = 0;
+                    minSumA = sumA;
+                }
+            }
+
+            int minPath = (minSumA + minSumB + t) > (sumB + t) ? (sumB + t) : (minSumA + minSumB + t);
+
+            Console.WriteLine(minPath);
+        }
+    }
+}
