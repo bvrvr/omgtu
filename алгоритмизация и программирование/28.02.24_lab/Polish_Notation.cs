c52dce0f07f4b1c2f073e71c78cac8fabb45a62f
Varvara Buryak <buryak.varvara@gmail.com>
2024-04-29 01:06:01 +0600

    Add files via upload

---
+++ алгоритмизация и программирование/28.02.24_lab/Polish_Notation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polish_Notation
+{
+    internal class Polish_Notation
+    {
+        static void Main(string[] args)
+        {
+            string expression = "5 6 * 9 +";
+            Console.WriteLine("Результат: " + Calculate(expression));
+        }
+
+        static int Calculate(string expression)
+        {
+            Stack<int> stack = new Stack<int>();
+            string[] tokens = expression.Split(' ');
+
+            foreach (string token in tokens)
+            {
+                if (int.TryParse(token, out int number))
+                {
+                    stack.Push(number);
+                }
+                else
+                {
+                    int operand2 = stack.Pop();
+                    int operand1 = stack.Pop();
+
+                    switch (token)
+                    {
+                        case "+":
+                            stack.Push(operand1 + operand2);
+                            break;
+                        case "-":
+                            stack.Push(operand1 - operand2);
+                            break;
+                        case "*":
+                            stack.Push(operand1 * operand2);
+                            break;
+                        case "/":
+                            if (operand2 == 0)
+                            {
+                                throw new ArgumentException("Попытка деления на ноль");
+                            }
+                            stack.Push(operand1 / operand2);
+                            break;
+                        default:
+                            throw new ArgumentException($"Недопустимый оператор: {token}");
+                    }
+                }
+            }
+
+            if (stack.Count != 1)
+            {
+                throw new ArgumentException("Неверное выражение");
+            }
+
+            return stack.Pop();
+        }
+    }
+}
