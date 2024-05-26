14990e80ea8e3aec76daacfa7e224f372b5d07df
Varvara Buryak <buryak.varvara@gmail.com>
2024-05-27 01:03:42 +0600

    Add files via upload

---
+++ алгоритмизация и программирование/семестр 2/18.03.24_lab/Delegates.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegates
+{
+    public class Car
+    {
+        public string CarMake { get; set; }
+
+        public Car(string carMake)
+        {
+            CarMake = carMake;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Машина марки: {CarMake}");
+        }
+    }
+    public class CarWash
+    {
+        public void Wash(Car car)
+        {
+            Console.WriteLine($"Мытьё машины марки: {car.CarMake}");
+        }
+    }
+    public class Garage
+    {
+        public List<Car> Cars { get; set; }
+
+        public Garage()
+        {
+            Cars = new List<Car>();
+        }
+
+        public void AddCar(Car car)
+        {
+            Cars.Add(car);
+        }
+
+        public delegate void CarWashDelegate(Car car);
+
+        public void WashAllCars(CarWashDelegate carWashDelegate)
+        {
+            foreach (var car in Cars)
+            {
+                carWashDelegate(car);
+            }
+        }
+    }
+    internal class Delegates
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("LADA");
+            Car car2 = new Car("Toyota");
+            Car car3 = new Car("Nissan");
+
+            Garage garage = new Garage();
+            garage.AddCar(car1);
+            garage.AddCar(car2);
+            garage.AddCar(car3);
+
+            CarWash carWash = new CarWash();
+
+            Garage.CarWashDelegate carWashDelegate = new Garage.CarWashDelegate(carWash.Wash);
+
+            garage.WashAllCars(carWashDelegate);
+        }
+    }
+}
