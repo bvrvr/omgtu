5a5e84d14263ab5d00fe57c5eadd87462dae87fb
Varvara Buryak <buryak.varvara@gmail.com>
2024-06-05 01:15:51 +0600

    Add files via upload

---
+++ алгоритмизация и программирование/семестр 2/24.04.24_lab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _24._04._24_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string> { "cat", "fish", "dolphin", "monkey", "hare", "elephant", "mouse", "turtle" };
+
+            var evenLengthStrings = strings.Where(s => s.Length % 2 == 0).ToList();
+            Console.WriteLine("Строки четной длины после первого запроса:");
+            evenLengthStrings.ForEach(Console.WriteLine);
+
+            for (int i = evenLengthStrings.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    evenLengthStrings.RemoveAt(i);
+                }
+            }
+
+            var filteredAgain = evenLengthStrings.Where(s => s.Length % 2 == 0).ToList();
+            Console.WriteLine("\nСтроки четной длины после второго запроса:");
+            filteredAgain.ForEach(Console.WriteLine);
+        }
+    }
+}
